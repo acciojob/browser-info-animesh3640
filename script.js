b88e48eacf0ff9d49827e7512e5332fb73922b0b
animesh3640 <127398605+animesh3640@users.noreply.github.com>
2023-05-01 19:43:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let browserName = navigator.appName;
+		let version = navigator.appVersion;
+
+		// Display browser information on the page
+		let browserInfoDiv = document.getElementById("browser-info");
+		browserInfoDiv.innerText = "You are using " + browserName + " version " + version;
